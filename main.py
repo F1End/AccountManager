077974614432498d4d305db7b4ae94da2937d1f7
F1End <horvirich@gmail.com>
2024-06-17 11:24:01 +0200

    -removing surplis lines, test comments and unsused import

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ import os.path as path
 
 from streamlit.web import cli as stcli
 
-from tools import request_builder
-
-
 
 if __name__ == '__main__':
     sys.argv = ["streamlit", "run", path.join("tools", "ui_st.py")]
     sys.exit(stcli.main())
-    # test = request_builder.SessionManager()
-    # test2 = test.db_scheme()
-    #

0953a06a63fabcd91678cb43edfa360224138f15
F1End <horvirich@gmail.com>
2024-06-25 23:31:17 +0200

    -Small cleanup of spaces/unused functiones/modules/etc

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os.path as path
 
 from streamlit.web import cli as stcli
 
-
 if __name__ == '__main__':
     sys.argv = ["streamlit", "run", path.join("tools", "ui_st.py")]
     sys.exit(stcli.main())
